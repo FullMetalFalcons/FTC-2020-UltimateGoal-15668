540048bef1915db30ccf05821e8b1b2589818edb
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 19:35:44 -0500

    major update to have the 3 tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autotest.java
@@ -0,0 +1,145 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+@Autonomous(name = "AUTOTEST", group = "Test")
+
+public class autotest extends  LinearOpMode {
+
+    DcMotor m1, m2, m3, m4;
+    BNO055IMU imu;
+    DistanceSensor frontDistance, leftDistance, rightDistance, backDistance;
+    ColorSensor colorSensor;
+
+    public void runOpMode(){
+
+        m1 = hardwareMap.dcMotor.get("back_left_motor");
+        m2 = hardwareMap.dcMotor.get("front_left_motor");
+        m3 = hardwareMap.dcMotor.get("front_right_motor");
+        m4 = hardwareMap.dcMotor.get("back_right_motor");
+        m1.setDirection(DcMotor.Direction.REVERSE);
+        m2.setDirection(DcMotor.Direction.REVERSE);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        frontDistance = hardwareMap.get(DistanceSensor.class, "front_distance");
+        leftDistance = hardwareMap.get(DistanceSensor.class, "left_distance");
+        rightDistance = hardwareMap.get(DistanceSensor.class, "right_distance");
+        backDistance = hardwareMap.get(DistanceSensor.class, "back_distance");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.accelerationIntegrationAlgorithm = null;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationData = null;
+        parameters.calibrationDataFile = "";
+        parameters.loggingEnabled = false;
+        parameters.loggingTag = "Who cares.";
+        imu.initialize(parameters);
+
+        //commands to run in auto starts
+
+        setPower(0, 1, 0);
+        while (m1.getCurrentPosition() < 2200) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 0, 0.5);
+        while (m1.getCurrentPosition() > 130) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 1, 0);
+        while (m1.getCurrentPosition() < 2400) {
+        }
+        setPower(0, 0, 0);
+
+        sleep(2000);
+
+        setPower(0, -1, 0);
+        while (m1.getCurrentPosition() > 130) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 0, -0.5);
+        while (m1.getCurrentPosition() < 2200) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 1, 0);
+        while (m1.getCurrentPosition() < 10500) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 0, 0.5);
+        while (m1.getCurrentPosition() > 8470) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 1, 0);
+        while (m1.getCurrentPosition() < 10750) {
+        }
+        setPower(0, 0, 0);
+
+        sleep(2000);
+
+        setPower(0, -1, 0);
+        while (m1.getCurrentPosition() > 8470) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 0, 0.5);
+        while (m1.getCurrentPosition() > 6380) {
+        }
+        setPower(0, 0, 0);
+
+        setPower(0, 1, 0);
+        while (m1.getCurrentPosition() < 17000) {
+        }
+        setPower(0, 0, 0);
+
+        //commands to run in auto ends
+
+        m1.setPower(0);
+        m2.setPower(0);
+        m3.setPower(0);
+        m4.setPower(0);
+
+    }
+
+    void setPower(double px, double py, double pa) {
+        double p1 = -px + py - pa;
+        double p2 = px + py + -pa;
+        double p3 = -px + py + pa;
+        double p4 = px + py + pa;
+        double max = Math.max(1.0, Math.abs(p1));
+        max = Math.max(max, Math.abs(p2));
+        max = Math.max(max, Math.abs(p3));
+        max = Math.max(max, Math.abs(p4));
+        p1 /= max;
+        p2 /= max;
+        p3 /= max;
+        p4 /= max;
+        m1.setPower(p1);
+        m2.setPower(p2);
+        m3.setPower(p3);
+        m4.setPower(p4);
+    }
+
+    void runEnc() {
+        m1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        m2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        m3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        m4.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    void resetEnc() {
+        m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specialtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specialtest.java
@@ -1,13 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -15,42 +9,38 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.*;
 
-@TeleOp(name = "test bot", group = "Mechanum")
-public class testbot extends LinearOpMode {
+@TeleOp(name = "Mech Test Code 2", group = "Test")
+
+public class specialtest extends LinearOpMode {
 
     public void runOpMode() {
-        Gyroscope imu = hardwareMap.get(Gyroscope.class, "imu");
-        DigitalChannel digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        DistanceSensor sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+
         DcMotor m1 = hardwareMap.dcMotor.get("back_left_motor");
         DcMotor m2 = hardwareMap.dcMotor.get("front_left_motor");
         DcMotor m3 = hardwareMap.dcMotor.get("front_right_motor");
         DcMotor m4 = hardwareMap.dcMotor.get("back_right_motor");
-        DcMotor m5 = hardwareMap.dcMotor.get("5");
-        DcMotor m6 = hardwareMap.dcMotor.get("6");
+        DcMotor m5 = hardwareMap.dcMotor.get("shooter_launch");
+        DcMotor m6 = hardwareMap.dcMotor.get("shooter_intake");
         DcMotor m7 = hardwareMap.dcMotor.get("7");
         DcMotor m8 = hardwareMap.dcMotor.get("8");
-        DcMotor m9 = hardwareMap.dcMotor.get("9");
-        DcMotor m10 = hardwareMap.dcMotor.get("10");
-        DcMotor m11 = hardwareMap.dcMotor.get("11");
+
+        m1.setDirection(DcMotor.Direction.REVERSE);
+        m2.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
+
         while (opModeIsActive()){
 
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-
             double px = gamepad1.left_stick_x;
-            if (Math.abs(px) < 0.05) px = 0;
             double py = -gamepad1.left_stick_y;
-            if (Math.abs(py) < 0.05) py = 0;
             double pa = -gamepad1.right_stick_x;
             if (Math.abs(pa) < 0.05) pa = 0;
             double p1 = -px + py - pa;
@@ -70,27 +60,32 @@ public class testbot extends LinearOpMode {
             m3.setPower(p3);
             m4.setPower(p4);
 
-            if (gamepad1.a == true) {
-                m5.setPower(100);
-                m6.setPower(100);
-                m7.setPower(100);
-                m8.setPower(100);
-                m9.setPower(100);
-                m10.setPower(100);
-                m11.setPower(100);
+            if (gamepad1.left_trigger >= 0.1) {
+                m5.setPower(-100);
             }
 
-            if (gamepad1.b == true) {
-                m5.setPower(-100);
+            if (gamepad1.right_trigger >= 0.1) {
+                m5.setPower(100);
+            }
+
+            if (gamepad1.left_bumper == true) {
                 m6.setPower(-100);
-                m7.setPower(-100);
-                m8.setPower(-100);
-                m9.setPower(-100);
-                m10.setPower(-100);
-                m11.setPower(-100);
+            }
+
+            if (gamepad1.right_bumper == true) {
+                m6.setPower(100);
             }
 
         }
+
+        m1.setPower(0);
+        m2.setPower(0);
+        m3.setPower(0);
+        m4.setPower(0);
+        m5.setPower(0);
+        m6.setPower(0);
+        m7.setPower(0);
+        m8.setPower(0);
 
     }
 
