a60a89b21bd0d52914014c128d35f36a68cffbb5
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 09:58:52 -0500

    fixed arm encoder telemetry, fixed motor encoder reset position in code, and added base for operating limits for the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specialtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specialtest.java
@@ -37,21 +37,26 @@ public class specialtest extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        waitForStart();
+        m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m5.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m6.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m7.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m8.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        waitForStart();
 
         while (opModeIsActive()){
 
             double y = gamepad1.left_stick_y;
             double x = -gamepad1.left_stick_x;
             double rx = -gamepad1.right_stick_x;
-            m2.setPower(y + x + rx);
             m1.setPower(y - x + rx);
+            m2.setPower(y + x + rx);
             m3.setPower(y - x - rx);
             m4.setPower(y + x - rx);
-
-
-            telemetry.addData("Encoders"," %d %d %d %d", m1.getCurrentPosition(), m2.getCurrentPosition(), m3.getCurrentPosition(), m4.getCurrentPosition());
 
             if (gamepad1.left_trigger >= 0.1) {
                 m5.setPower(-gamepad1.left_trigger);
@@ -69,10 +74,11 @@ public class specialtest extends LinearOpMode {
                 m6.setPower(0);
             }
 
-
-            if (gamepad1.y == true) {
+            double minarm = 0;
+            double maxarm = 999999999;
+            if ((gamepad1.y == true) && (m7.getCurrentPosition() <= maxarm)) {
                 m7.setPower(0.3);
-            } else if (gamepad1.b == true) {
+            } else if ((gamepad1.b == true) && (m7.getCurrentPosition() >= minarm)) {
                 m7.setPower(-0.3);
             } else {
                 m7.setPower(0);
@@ -86,14 +92,14 @@ public class specialtest extends LinearOpMode {
                 m8.setPower(0);
             }
 
-
             if (gamepad1.dpad_up) {
                 m9.setPosition(0);
             } else if (gamepad1.dpad_right) {
                 m9.setPosition(0.62);
             }
 
-            telemetry.addData("arm: ", m8.getCurrentPosition());
+            telemetry.addData("Encoders"," %d %d %d %d", m1.getCurrentPosition(), m2.getCurrentPosition(), m3.getCurrentPosition(), m4.getCurrentPosition());
+            telemetry.addData("arm: ", m7.getCurrentPosition());
             telemetry.update();
 
         }
@@ -106,14 +112,6 @@ public class specialtest extends LinearOpMode {
         m6.setPower(0);
         m7.setPower(0);
         m8.setPower(0);
-        m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m5.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m6.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m7.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m8.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
     }
 
