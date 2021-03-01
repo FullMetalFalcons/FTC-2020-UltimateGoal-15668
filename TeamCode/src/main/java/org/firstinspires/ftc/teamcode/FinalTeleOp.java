2767ebe63d02a621c867fb880ee06f7b300bdd24
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 13:02:55 -0500

    remove unused importts for teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
@@ -6,9 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 // teleop is defined
 @TeleOp(name = "TeleOp", group = "Final")
